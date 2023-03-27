Fix uncertainty calculation once more

src/digitalworker_plot_and_metrics.py
@@ -229,7 +229,9 @@ if __name__ == '__main__':
     # print(y_test)
 
     # Read uncertainty
-    y_pred_std = pd.read_csv(y_pred_std_file, index_col=0).to_numpy().flatten()
+    # y_pred_std = pd.read_csv(y_pred_std_file, index_col=0).to_numpy().flatten()
+    y_pred_std = pd.read_csv(y_pred_std_file, index_col=0).to_numpy()
+    y_pred_std = np.mean(y_pred_std, axis=1)
 
     # plot_confusion_for_paper(y_test, y_pred)
     calculate_metrics(y_test, y_pred, y_pred_std)

src/evaluate.py
@@ -313,7 +313,8 @@ def evaluate(model_filepath, train_filepath, test_filepath, calibrate_filepath):
 
                 predictions = np.stack(predictions, -1)
                 mean = np.mean(predictions, axis=-1)
-                std = - 1.0 * np.sum(mean * np.log(mean + 1e-15), axis=-1)
+                # std = - 1.0 * np.sum(mean * np.log(mean + 1e-15), axis=-1)
+                std = - np.sum(predictions * np.log(predictions + 1e-15), axis=-1)
 
                 y_pred = mean
                 y_pred_std = std
@@ -556,6 +557,10 @@ def plot_confusion(y_test, y_pred, y_pred_std=None):
     plt.savefig(PLOTS_PATH / "confusion_matrix.png")
 
     if y_pred_std is not None:
+
+        # Take the average of the entropy per class
+        y_pred_std = np.mean(y_pred_std, axis=1)
+
         cm = coo_matrix(
                 (y_pred_std, (y_test, y_pred)),
                 shape=(n_labels, n_labels)